adc5d9075ae12f0329bbb0ec01d629587b8fa682
Klithik <santi17461@gmail.com>
2025-11-23 14:39:15 -0300

    created flag arguments

---
+++ cmd/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	var strat, source_dir, end_dir string
+	var ignore_hidden bool
+
+	const stratDoc string = "What should the folder be sorted by: the possible values are:\n-year: in order to place all valid files inside a year of creation folder\n-type: in order to place all valid files inside a folder based on its filetype (it tries to determine the file type based on magic bytes, if it fails, it uses the file extension)"
+	flag.StringVar(&strat, "strategy", "year", stratDoc)
+
+	const source_dirDoc string = "The directory to be sorted, by default it uses the current directory"
+	flag.StringVar(&source_dir, "source", ".", source_dirDoc)
+
+	const end_dirDoc string = "The directory where the folders to be created should be placed, by default it uses the current directory"
+	flag.StringVar(&end_dir, "end", ".", end_dirDoc)
+
+	const ignoreHiddenDoc string = "Ignores hidden files (files starting with a '.' character)"
+	flag.BoolVar(&ignore_hidden, "ignore_hidden", true, ignoreHiddenDoc)
+
+	flag.Parse()
+
+	fmt.Println(strat)
+}
