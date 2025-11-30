ba434d1e9b85e1f0d41c3f903dc89150f8e811b3
Klithik <santi17461@gmail.com>
2025-11-29 23:18:44 -0300

    first creation date function for windows

--- internal/scanner/scan_windows.go
+++ internal/scanner/scan_windows.go
@@ -3,9 +3,19 @@
 package scanner
 
 import (
+	"fmt"
+	"os"
+	"syscall"
 	"time"
 )
 
 func obtainCreationDate(file string) (time.Time, error) {
-	return time.Time{}, nil
+	info, err := os.Stat(file)
+	if err != nil {
+		fmt.Printf("Failed to obtain %v creation date\n", file)
+		return time.Time{}, err
+	}
+	sysData := info.Sys().(*syscall.Win32FileAttributeData)
+	creation := time.Unix(0, sysData.CreationTime.Nanoseconds())
+	return creation, nil
 }
